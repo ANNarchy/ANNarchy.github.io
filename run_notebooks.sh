fa187bdd6cfc98e2ff99ae47668b008bfb9b8d99
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2026-01-06 06:31:52 +0100

    Updated run_notebooks script.

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+KERNEL_NAME=base        # which conda environment should be used
+KERNEL_TIMEOUT=60       # max time for an individual cell
 
 for nb in notebooks/*.ipynb; do
+  mkdir notebooks/runs
   echo "Running $nb"
-  jupyter nbconvert --to notebook --execute --inplace "$nb"
+  jupyter nbconvert --ExecutePreprocessor.kernel_name=$KERNEL_NAME --ExecutePreprocessor.timeout=$KERNEL_TIMEOUT --to notebook --execute --inplace "$nb"
   rm -rf notebooks/annarchy notebooks/runs
 done
